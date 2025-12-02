de68fa3ba63464cd410eaf764008c7ccf5906cd4
SumitKumar8252 <sk497623@gmail.com>
2025-12-03 01:37:22 +0530

    backend

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,15 +1,17 @@
 import Note from "../models/Note.js";
 
-export async function getAllNotes(_, res) {
+// Get all notes
+export async function getAllNotes(req, res) {
   try {
-    const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort in desc. order (newest first)
+    const notes = await Note.find().sort({ createdAt: -1 }); // sort by newest first
     res.status(200).json(notes);
   } catch (error) {
-    console.error("Error in getAllNotes controller", error);
+    console.error("Error in getAllNotes ", error);
     res.status(500).json({ message: "Internal server error" });
   }
 }
 
+// Get notes by id
 export async function getNoteById(req, res) {
   try {
     const note = await Note.findById(req.params.id);
@@ -21,6 +23,7 @@ export async function getNoteById(req, res) {
   }
 }
 
+// Create a new note
 export async function createNote(req, res) {
   try {
     const { title, content } = req.body;
@@ -34,6 +37,7 @@ export async function createNote(req, res) {
   }
 }
 
+// Update a note
 export async function updateNote(req, res) {
   try {
     const { title, content } = req.body;
@@ -54,6 +58,7 @@ export async function updateNote(req, res) {
   }
 }
 
+// Delete a note
 export async function deleteNote(req, res) {
   try {
     const deletedNote = await Note.findByIdAndDelete(req.params.id);
